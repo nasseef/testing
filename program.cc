40294b08106969246d515aadf732564f33f9f112
Nasseef Abukamail <abukamai@ohio.edu>
2021-11-09 12:51:07 -0500

    Created the hello program

---
+++ program.cc
@@ -0,0 +1,19 @@
+/**
+ *   @file: program.cc
+ * @author: Nasseef Abukamail
+ *   @date: November 09, 2021
+ *  @brief: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+///Constants and function prototypes
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello World"<< endl;
+    return 0;
+} /// main
